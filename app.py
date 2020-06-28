3dd8ab8269c0a173456ff2ab12ea8c3e2f7a749f
zkdlin <qkrekqls5120@naver.com>
2020-06-28 23:36:49 +0900

    SQLlite DB 소스 적용

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, render_template, redirect, url_for, abort
 import game
 import json
 
-import db.db
+import dbdb
 
 app = Flask(__name__)
 
@@ -71,16 +71,15 @@ def method():
         num = request.form['num']
         name = request.form['name']
         print(num, name)
-        with open("static/save.txt","w", encoding='utf-8') as f:
-            f.write("%s,%s" % (num, name))
+        dbdb.insert_data(num, name)
         return 'POST 이다. 학번은 : {} 이름은 : {}'.format(num, name)
 
 @app.route('/getinfo')
 def getinfo():
-    # 파일 입력
-    with open("static/save.txt", "r", encoding='utf-8') as file:
-        student = file.read().split(',')  # 쉽표로 잘라서 student 에 배열로 저장
-    return '번호 : {}, 이름 : {}'.format(student[0], student[1])
+    ret = dbdb.select_all()
+    print(ret[2])
+    return render_template('getinfo.html', deat = ret)
+    #return '번호 : {}, 이름 : {}'.format(ret[0], ret[1])
 
 # 'redirect' = url로 페이지를 이동시킴 
 @app.route('/naver')
